ff15d55ed4a9a4141e932cddb7b794a829068cfb
Галеев Ильдар <ildar-galeev92@mail.ru>
2024-05-11 17:35:15 +0700

    add model Product

--- trade_network/models/__init__.py
+++ trade_network/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     "Supplier",
     "Contact",
+    "Product",
 ]
 
 from .supplier import Supplier
 from .contact import Contact
+from .product import Product
---
+++ trade_network/models/product.py
@@ -0,0 +1,25 @@
+# Create your models here.
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название",
+    )
+
+    model = models.CharField(
+        max_length=150,
+        verbose_name="Модель",
+    )
+
+    release_date = models.DateField(
+        auto_now=False,
+        verbose_name="Дата выхода продукта на рынок"
+    )
+
+    dealers = models.ManyToManyField(
+        to="Supplier",
+        related_name="products",
+        verbose_name="Дилеры",
+    )
