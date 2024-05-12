22cc5d12e563c44b144f9c884656c1aab10ef911
Галеев Ильдар <ildar-galeev92@mail.ru>
2024-05-12 17:21:10 +0700

    add drf CRUD Supplier

--- trade_network/urls.py
+++ trade_network/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from trade_network.apps import TradeNetworkConfig
+from trade_network.views import SupplierViewSet
 
 router = DefaultRouter()
+router.register('supplier', SupplierViewSet)
 
 app_name = TradeNetworkConfig.name
 
--- trade_network/views.py
+++ trade_network/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from trade_network.models import Supplier
+from trade_network.serializers import (
+    SupplierSerializer,
+    DetailSupplierSerializer,
+    EditSupplierSerializer,
+)
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(DetailSupplierSerializer(instance).data)
+
+    def get_serializer_class(self):
+
+        if self.action == 'list':
+            return SupplierSerializer
+        if self.action == 'retrieve':
+            return DetailSupplierSerializer
+        if self.action in ['update', 'partial_update']:
+            return EditSupplierSerializer
+        return SupplierSerializer
